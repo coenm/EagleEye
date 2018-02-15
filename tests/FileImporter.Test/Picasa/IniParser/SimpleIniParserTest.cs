4ee34f60bff307176d630cedcdd401e87dd69b51
Coen Munckhof <c.munckhof@gmail.com>
2018-02-15 23:14:40 +0100

    Added test.

--- tests/FileImporter.Test/Picasa/IniParser/SimpleIniParserTest.cs
+++ tests/FileImporter.Test/Picasa/IniParser/SimpleIniParserTest.cs
@@ -1,6 +1,10 @@
 ﻿namespace EagleEye.FileImporter.Test.Picasa.IniParser
 {
     using System;
+    using System.IO;
+    using System.Text;
+
+    using FluentAssertions;
 
     using Xunit;
 
@@ -12,6 +16,25 @@
         public void ParseNullStreamShouldThrowArgumentNullExceptionTest()
         {
             Assert.Throws<ArgumentNullException>(() => Sut.Parse(null));
+        }
+
+        [Fact]
+        public void EmptyIniFileShouldResultInAnEmptyResultTest()
+        {
+            // arrange
+            using (var stream = GenerateStreamFromString(string.Empty))
+            {
+                // act
+                var result = Sut.Parse(stream);
+
+                // assert
+                result.Should().BeEmpty();
+            }
+        }
+
+        private static MemoryStream GenerateStreamFromString(string value)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(value ?? string.Empty));
         }
     }
 }
