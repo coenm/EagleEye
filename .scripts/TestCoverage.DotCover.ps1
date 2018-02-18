920ddd2d6857c450f2f71645e8b60926aab1ab64
Coen Munckhof <c.munckhof@gmail.com>
2018-02-18 23:52:03 +0100

    dotcover retry

--- .scripts/TestCoverage.DotCover.ps1
+++ .scripts/TestCoverage.DotCover.ps1
@@ -23,7 +23,6 @@ $dotnetExe = 'C:\Program Files\dotnet\dotnet.exe'
 
 $outputOpenCoverXmlFile = 'C:\projects\eagleeye\coverage-dotnet.xml'
 $outputOpenCoverXmlFile = (join-path $RootDir "coverage-dotnet.xml")
-$slnFile = (join-path $RootDir "EagleEye.sln")
 
 Write-Host "Location dotcover.exe: " $dotcoverExe
 Write-Host "Location dotnet.exe: " $dotnetExe
@@ -38,50 +37,43 @@ $dotcoverFilter = "+:type=EagleEye.*;-:module=*.Test;"
 $testProjectLocations = Get-ChildItem -Recurse | Where-Object{$_.Name -like "*Test.csproj" } | % { $_.FullName }; # access $_.DirectoryName for the directory.
 # popd
 
+Try
+{
+	ForEach ($testProjectLocation in $testProjectLocations)
+	{
+		Write-Host "Run tests for project " (Resolve-Path $testProjectLocation).Path;
 
-$command = $dotcoverExe + ' analyse /TargetExecutable="' + $dotnetExe + '" /TargetArguments="test ' + $slnFile + ' '+ $dotnetTestArgs + '" "/Output=' + $outputOpenCoverXmlFile + '" /Filters="' +  $dotcoverFilter + '"'
+# "C:\Users\coen\AppData\Local\JetBrains\Installations\dotCover11\dotCover.exe" analyse /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="test C:\Repositories\EagleEye\EagleEye.sln" /Filters=+:type=EagleEye.*;-:module=*.Test; /Output="D:/output.html" /ReportType="HTML"  
 		
-Write-Output $command
+		$command = $dotcoverExe + ' analyse /TargetExecutable="' + $dotnetExe + '" /TargetArguments="test ' + $testProjectLocation + ' '+ $dotnetTestArgs + '" "/Output=' + $testProjectLocation + '.coverage-dotnet.xml" /Filters="' +  $dotcoverFilter + '"'
 		
-iex $command
-
-# Try
-# {
-# 	ForEach ($testProjectLocation in $testProjectLocations)
-# 	{
-# 		Write-Host "Run tests for project " (Resolve-Path $testProjectLocation).Path;
-# 
-# # "C:\Users\coen\AppData\Local\JetBrains\Installations\dotCover11\dotCover.exe" analyse /TargetExecutable="C:\Program Files\dotnet\dotnet.exe" /TargetArguments="test C:\Repositories\EagleEye\EagleEye.sln" /Filters=+:type=EagleEye.*;-:module=*.Test; /Output="D:/output.html" /ReportType="HTML"  
-# 		
-# 		$command = $dotcoverExe + ' analyse /TargetExecutable="' + $dotnetExe + '" /TargetArguments="test ' + $testProjectLocation + ' '+ $dotnetTestArgs + '" "/Output=' + $outputOpenCoverXmlFile + '" /Filters="' +  $dotcoverFilter + '"'
-# 		
-# 		Write-Output $command
-# 		
-# 		iex $command
-# 		
-# 		Write-Host "Command finished, ready for the next one"
-# 	}
-# 
-# 	# Either display or publish the results
-# 	If ($env:CI -eq 'True')
-# 	{
-# 		Write-Output "Running from CI"
-# #		$command = (Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe' + ' --opencover "' + $outputFile + '" --full-sources'
-# #		Write-Output $command
-# #		iex $command
-# 	}
-# 	Else
-# 	{
-# 		Write-Output "Running local"
-# #		$command = (Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\ReportGenerator'))[0].FullName + '\tools\ReportGenerator.exe -reports:"' + $outputFile + '" -targetdir:"' + $outputPath + '"'
-# #		Write-Output $command
-# #		iex $command
-# #		cd $outputPath
-# #		./index.htm
-# 	}
-# }
-# Finally
-# {
-# 	Write-Output "Finally"
-# 	popd
-# }
+		Write-Output $command
+		
+		iex $command
+		
+		Write-Host "Command finished, ready for the next one"
+	}
+	
+	# Either display or publish the results
+	If ($env:CI -eq 'True')
+	{
+		Write-Output "Running from CI"
+#		$command = (Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe' + ' --opencover "' + $outputFile + '" --full-sources'
+#		Write-Output $command
+#		iex $command
+	}
+	Else
+	{
+		Write-Output "Running local"
+#		$command = (Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\ReportGenerator'))[0].FullName + '\tools\ReportGenerator.exe -reports:"' + $outputFile + '" -targetdir:"' + $outputPath + '"'
+#		Write-Output $command
+#		iex $command
+#		cd $outputPath
+#		./index.htm
+	}
+}
+Finally
+{
+	Write-Output "Finally"
+	popd
+}
