2b61d95717c3f24e6e3ba52bc07b275995603f0e
Coen Munckhof <c.munckhof@gmail.com>
2018-03-02 18:13:38 +0100

    Ready for linux?

---
+++ src/ExifToolWrapper/ExifToolExecutable.cs
@@ -0,0 +1,31 @@
+﻿namespace EagleEye.ExifToolWrapper
+{
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    public static class ExifToolExecutable
+    {
+        public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+
+        public static string NewLine
+        {
+            get
+            {
+                if (IsWindows)
+                    return "\r\n";
+                return "\n";
+            }
+        }
+        public static byte[] NewLineBytes => Encoding.ASCII.GetBytes(NewLine);
+
+        public static string GetExecutableName()
+        {
+            if (IsLinux)
+                return "exiftool";
+            return "exiftool.exe";
+        }
+    }
+}
