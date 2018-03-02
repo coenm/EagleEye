af3a5a8957896628a7000962bcda171a6b1602a7
Coen Munckhof <c.munckhof@gmail.com>
2018-03-02 20:14:37 +0100

    exiftool version.

--- .scripts/InstallExiftool.sh
+++ .scripts/InstallExiftool.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # sudo apt-get install perl
 
+EXIFTOOL_VERSION_READ=`cat ../EXIFTOOL_VERSION`
+echo "Read exiftool version: ${EXIFTOOL_VERSION_READ}"
+
 wget https://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz
 gzip -dc Image-ExifTool-${EXIFTOOL_VERSION}.tar.gz | tar -xf -
 cd Image-ExifTool-${EXIFTOOL_VERSION}
