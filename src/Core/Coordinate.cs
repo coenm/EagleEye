175c5e1d840cc8f15eb1124fafb0c5e89dd475be
Coen Munckhof <c.munckhof@gmail.com>
2018-03-14 23:31:35 +0100

    Remove IEquatable from Coordinate

--- src/Core/Coordinate.cs
+++ src/Core/Coordinate.cs
@@ -1,7 +1,6 @@
 ﻿namespace EagleEye.Core
 {
-    using System;
-    public class Coordinate : IEquatable<Coordinate>
+    public class Coordinate
     {
         public Coordinate(float latitude, float longitude)
         {
@@ -12,33 +11,5 @@
         public float Latitude { get; }
 
         public float Longitude { get; }
-
-        public bool Equals(Coordinate other)
-        {
-            if (other is null)
-                return false;
-            if (ReferenceEquals(this, other))
-                return true;
-            return Latitude.Equals(other.Latitude) && Longitude.Equals(other.Longitude);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is null)
-                return false;
-            if (ReferenceEquals(this, obj))
-                return true;
-            if (obj.GetType() != GetType())
-                return false;
-            return Equals((Coordinate)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (Latitude.GetHashCode() * 397) ^ Longitude.GetHashCode();
-            }
-        }
     }
 }
