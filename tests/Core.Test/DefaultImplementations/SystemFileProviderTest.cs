7bfb00d0ba50915748a2a7722ae128fd56f27a74
Coen Munckhof <c.munckhof@gmail.com>
2018-03-22 22:38:32 +0100

    Added test.

---
+++ tests/Core.Test/DefaultImplementations/SystemFileProviderTest.cs
@@ -0,0 +1,49 @@
+﻿namespace EagleEye.Core.Test.DefaultImplementations
+{
+    using System.IO;
+
+    using EagleEye.Core.DefaultImplementations;
+    using EagleEye.TestHelper;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class SystemFileServiceTest
+    {
+        private readonly SystemFileService _sut;
+        private readonly string _filename;
+
+        public SystemFileServiceTest()
+        {
+            _sut = SystemFileService.Instance;
+            _filename = Path.Combine(TestImages.InputImagesDirectoryFullPath, "1.jpg");
+        }
+
+        [Fact]
+        public void FileExistsTest()
+        {
+            // arrange
+
+            // act
+            var result = _sut.FileExists(_filename);
+
+            // assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void OpenReadTest()
+        {
+            // arrange
+            var filename = Path.Combine(_filename);
+
+            // act
+            using (var result = _sut.OpenRead(filename))
+            {
+                // assert
+                result.Should().NotBeNull();
+            }
+        }
+    }
+}
--- tests/LuceneNet.Test/Facet/NumberFacetResult.cs
+++ tests/LuceneNet.Test/Facet/NumberFacetResult.cs
@@ -21,7 +21,7 @@
 
             public override bool Equals(object obj)
             {
-                if (ReferenceEquals(null, obj))
+                if (obj is null)
                     return false;
 
                 if (ReferenceEquals(this, obj))
