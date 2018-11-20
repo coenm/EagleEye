b1b60ae046e110171fa9dd78e2b2e23f31b82592
Coen Munckhof <c.munckhof@gmail.com>
2018-11-20 21:26:06 +0100

    Improve test for inmemory with SqLite

--- tests/Core.Test/ReadModel/ExploringEntityFrameworkTests.cs
+++ tests/Core.Test/ReadModel/ExploringEntityFrameworkTests.cs
@@ -3,9 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
-    using Core.ReadModel.EntityFramework.ContextOptions;
+
     using EagleEye.Core.ReadModel.EntityFramework;
     using EagleEye.Core.ReadModel.EntityFramework.Models;
     using FluentAssertions;
@@ -19,15 +18,13 @@
         public async Task SelectUsingPredicateFromEmptyDatabaseShouldReturnNothingTest()
         {
             // arrange
-            var sut = new InMemoryEagleEyeDbContextFactory();
-
-            using (var db = sut.CreateMediaItemDbContext())
+            using (var ctx = await CreateMediaItemDbContext())
             {
                 // act
-                var result = await db.Photos
-                                     .Where(item => item.EventTimestamp <= DateTimeOffset.UtcNow)
-                                     .ToListAsync()
-                                     .ConfigureAwait(false);
+                var result = await ctx.Photos
+                    .Where(item => item.EventTimestamp <= DateTimeOffset.UtcNow)
+                    .ToListAsync()
+                    .ConfigureAwait(false);
 
                 // assert
                 result.Should().BeEmpty();
@@ -43,59 +40,53 @@
             var item2 = Create(2, "abc2", DateTimeOffset.UtcNow); // should match predicate
             var item3 = Create(3, "abc3", DateTimeOffset.UtcNow.AddDays(1)); // should NOT match predicate
 
-            var sut = new InMemoryEagleEyeDbContextFactory();
-
-            using (var db = sut.CreateMediaItemDbContext())
+            using (var ctx = await CreateMediaItemDbContext())
             {
-                await db.Photos.AddRangeAsync(item1, item2, item3).ConfigureAwait(false);
-                await db.SaveChangesAsync().ConfigureAwait(false);
+                await ctx.Photos.AddRangeAsync(item1, item2, item3).ConfigureAwait(false);
+                await ctx.SaveChangesAsync().ConfigureAwait(false);
 
                 // act
-                var result = await db.Photos
-                                     .Where(item => item.EventTimestamp <= DateTimeOffset.UtcNow)
-                                     .ToListAsync()
-                                     .ConfigureAwait(false);
+                var result = await ctx.Photos
+                    .Where(item => item.EventTimestamp <= DateTimeOffset.UtcNow)
+                    .ToListAsync()
+                    .ConfigureAwait(false);
 
                 // assert
-                result.Should().BeEquivalentTo(new[] { item1, item2 }.AsEnumerable());
+                result.Should().BeEquivalentTo(new[] {item1, item2}.AsEnumerable());
             }
         }
 
         [Fact]
         [Xunit.Categories.Exploratory]
-        public async Task Abc()
+        public async Task InsertWithRelationTest()
         {
             // arrange
             var item1 = Create(1, "aaa", DateTimeOffset.UtcNow);
             item1.Tags = new List<Tag>
             {
-                new Tag { Value = "Vacation" },
-                new Tag { Value = "Summer" },
+                new Tag {Value = "Vacation"},
+                new Tag {Value = "Summer"},
             };
 
             var item2 = Create(1, "abcd", DateTimeOffset.UtcNow);
             item2.Tags = new List<Tag>
             {
-                new Tag { Value = "Vacation" },
-                new Tag { Value = "Winter" },
+                new Tag {Value = "Vacation"},
+                new Tag {Value = "Winter"},
             };
 
-            var sut = new InMemoryEagleEyeDbContextFactory();
-            using (var db = sut.CreateMediaItemDbContext())
-//            using (var db = CreateContext())
+            using (var ctx = await CreateMediaItemDbContext())
             {
-                await db.EnsureCreated();
-
-                await db.Photos.AddRangeAsync(item1, item2).ConfigureAwait(false);
-                await db.SaveChangesAsync().ConfigureAwait(false);
+                await ctx.Photos.AddRangeAsync(item1, item2).ConfigureAwait(false);
+                await ctx.SaveChangesAsync().ConfigureAwait(false);
 
                 // act
-                var result = await db.Photos
+                var result = await ctx.Photos
                     .ToListAsync()
                     .ConfigureAwait(false);
 
                 // assert
-                result.Should().BeEquivalentTo(new[] { item1, item2 }.AsEnumerable());
+                result.Should().BeEquivalentTo(new[] {item1, item2}.AsEnumerable());
             }
         }
 
@@ -106,28 +97,31 @@
                 Id = Guid.NewGuid(),
                 Filename = filename,
                 Version = version,
-                FileSha256 = new byte[1] { 0x01 },
+                FileSha256 = new byte[1] {0x01},
                 EventTimestamp = timestamp,
             };
         }
 
-        private EagleEyeDbContext CreateContext()
+        private async Task<EagleEyeDbContext> CreateMediaItemDbContext()
         {
-            var x = new DbContextOptionsFactory(new IDbContextOptionsStrategy[] {new InMemoryDatabaseOptionsBuilder(), new SqlLiteDatabaseOptionsBuilder() });
-            var xy = x.Create("Filename=./coenm.db");
-            var z = new EagleEyeDbContextFactory(xy);
-            var result = z.CreateMediaItemDbContext();
-            result.EnsureCreated().GetAwaiter().GetResult();
-            return result;
+            var sut = new InMemoryEagleEyeDbContextFactory();
+            var db = sut.CreateMediaItemDbContext();
+
+            // todo, move this
+            // because it is Sql Lite InMemory.
+            await db.Database.OpenConnectionAsync();
+            await db.Database.EnsureCreatedAsync();
+
+            return db;
         }
 
         internal class InMemoryEagleEyeDbContextFactory : EagleEyeDbContextFactory
         {
-            public InMemoryEagleEyeDbContextFactory([CallerMemberName] string name = "dummy")
+            public InMemoryEagleEyeDbContextFactory()
                 : base(new DbContextOptionsBuilder<EagleEyeDbContext>()
-                       .UseInMemoryDatabase(name)
-//                       .UseSqlite("Data Source=:memory:;")
-                       .Options)
+                    // .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .UseSqlite("Data Source=:memory:;")
+                    .Options)
             {
             }
         }
