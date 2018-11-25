59e29484890d29b708e64999ff0426b1f94c6fab
Coen Munckhof <c.munckhof@gmail.com>
2018-11-25 10:32:35 +0100

    remove old code

--- src/SearchEngine.Interface/Queries/SearchResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SearchEngine.Interface.Queries
-{
-    public class SearchResult
-    {
-        public int Count { get; set; }
-
-        public SearchItem[] Items { get; set; }
-    }
-}
