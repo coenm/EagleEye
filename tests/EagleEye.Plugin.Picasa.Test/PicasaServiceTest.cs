72b2234c812b9563de1a4095a834e22e33a5142d
Coen Munckhof <c.munckhof@gmail.com>
2019-02-22 22:07:42 +0100

    Picasa becomes a plugin

--- tests/EagleEye.Plugin.Picasa.Test/PicasaServiceTest.cs
+++ tests/EagleEye.Plugin.Picasa.Test/PicasaServiceTest.cs
@@ -6,7 +6,6 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    using EagleEye.Core.Interfaces;
     using EagleEye.Core.Interfaces.Core;
     using EagleEye.Picasa.Picasa;
     using FakeItEasy;
