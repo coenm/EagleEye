08561df6a9eaa16674f25242efda9ed93ae19eef
Coen Munckhof <c.munckhof@gmail.com>
2019-02-24 21:05:52 +0100

    Start of ExifTool plugin.

---
+++ src/EagleEye.Plugin.ExifTool/ExifToolPlugin.cs
@@ -0,0 +1,20 @@
+﻿namespace EagleEye.ExifTool
+{
+    using EagleEye.Core.Interfaces.Module;
+    using Helpers.Guards;
+    using JetBrains.Annotations;
+    using SimpleInjector;
+
+    internal class ExifToolPlugin : IEagleEyePlugin
+    {
+        public string Name => "PicasaPlugin";
+
+        public void EnablePlugin([NotNull] Container container)
+        {
+            Guard.NotNull(container, nameof(container));
+
+//            container.Register<IPicasaService, PicasaService>();
+//            container.Collection.Append(typeof(IPhotoPersonProvider), typeof(PicasaPersonProvider));
+        }
+    }
+}
---
+++ src/EagleEye.Plugin.ExifTool/InternalsVisibleTo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("EagleEye.Plugin.ExifTool.Test")]
+
+// required for FakeItEasy
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
