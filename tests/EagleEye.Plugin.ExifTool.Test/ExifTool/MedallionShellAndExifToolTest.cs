7b354cc6b48294a213b75e7968ad8a89e47b9027
Coen Munckhof <c.munckhof@gmail.com>
2019-02-26 22:51:52 +0100

    Use FodyHelper packge (appveyor linux)

--- tests/EagleEye.Plugin.ExifTool.Test/ExifTool/MedallionShellAndExifToolTest.cs
+++ tests/EagleEye.Plugin.ExifTool.Test/ExifTool/MedallionShellAndExifToolTest.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+
     using EagleEye.ExifTool;
     using EagleEye.ExifTool.ExifTool;
     using EagleEye.TestHelper;
