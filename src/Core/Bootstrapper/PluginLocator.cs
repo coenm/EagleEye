c29b4a641804c35246957c9b4a18b69cabca90c4
Coen Munckhof <c.munckhof@gmail.com>
2019-02-28 23:35:31 +0100

    bootstrap in core.

---
+++ src/Core/Bootstrapper/PluginLocator.cs
@@ -0,0 +1,57 @@
+﻿namespace EagleEye.Core.Bootstrapper
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    using Helpers.Guards;
+    using JetBrains.Annotations;
+    using NLog;
+
+    public static class PluginLocator
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        [NotNull]
+        public static IEnumerable<Assembly> FindPluginAssemblies([NotNull] string baseDirectory)
+        {
+            Guard.NotNullOrWhiteSpace(baseDirectory, nameof(baseDirectory));
+
+            Logger.Debug(() => $"Plugin base directory {baseDirectory}");
+
+            var assemblies = GetPluginAssembliesInDirectory(baseDirectory);
+
+            foreach (var dir in GetPluginDirectories(baseDirectory))
+            {
+                assemblies = assemblies.Concat(GetPluginAssembliesInDirectory(dir));
+            }
+
+            return assemblies;
+        }
+
+        private static IEnumerable<string> GetPluginDirectories(string baseDirectory)
+        {
+            var pluginBaseDirectory = Path.Combine(baseDirectory, "Plugins");
+
+            if (!Directory.Exists(pluginBaseDirectory))
+                return Enumerable.Empty<string>();
+
+            return Directory.EnumerateDirectories(pluginBaseDirectory);
+        }
+
+        [NotNull]
+        private static IEnumerable<Assembly> GetPluginAssembliesInDirectory([NotNull] string baseDirectory)
+        {
+            DebugGuard.NotNullOrWhiteSpace(baseDirectory, nameof(baseDirectory));
+
+            return new DirectoryInfo(baseDirectory)
+                .GetFiles()
+                .Where(file =>
+                    file.Name.StartsWith("EagleEye.Plugin.")
+                    &&
+                    file.Extension.ToLower() == ".dll")
+                .Select(file => Assembly.Load(AssemblyName.GetAssemblyName(file.FullName)));
+        }
+    }
+}
---
+++ tests/Core.Test/Bootstrapper/BootstrapperTest.cs
@@ -0,0 +1,44 @@
+﻿namespace EagleEye.Core.Test.Bootstrapper
+{
+    using System.Linq;
+
+    using FluentAssertions;
+    using JetBrains.Annotations;
+    using SimpleInjector;
+    using Xunit;
+
+    using Sut = EagleEye.Core.Bootstrapper.Bootstrapper;
+
+    public class BootstrapperTest
+    {
+        [NotNull] private static readonly Container Container = new Container();
+
+        /// <summary>
+        /// This test depends on the dependency projects.
+        /// </summary>
+        [Fact]
+        public void FindAvailablePlugins_ShouldReturnListOfFoundPlugins()
+        {
+            // arrange
+
+            // act
+            var result = Sut.FindAvailablePlugins().ToList();
+
+            // assert
+            result.Select(x => x.Name).Should().BeEquivalentTo("DirectoryStructurePlugin", "ExifToolPlugin", "ImageHashPlugin", "PicasaPlugin");
+        }
+
+        [Fact]
+        public void Bootstrap_ShouldNotThrow_WIP()
+        {
+            // arrange
+
+            // act
+            var plugins = Sut.FindAvailablePlugins();
+            Sut.Bootstrap(Container, plugins);
+
+            // assert
+            Container.Verify(VerificationOption.VerifyAndDiagnose);
+        }
+    }
+}
