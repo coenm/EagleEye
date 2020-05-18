f6c3ebc0109d8ea9e3d23d19decd8ab34620f6a4
Coen van den Munckhof <c.munckhof@gmail.com>
2020-05-18 23:30:58 +0200

    Indexing files.

---
+++ src/FileImporter/CmdOptions/IndexFilesOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace EagleEye.FileImporter.CmdOptions
+{
+    using CommandLine;
+    using JetBrains.Annotations;
+
+    [Verb("index", HelpText = "Index files")]
+    [UsedImplicitly]
+    internal class IndexFilesOptions
+    {
+        [Option(shortName: 'd', longName: "directory", Required = true)]
+        public string Directory { get; set; }
+    }
+}
